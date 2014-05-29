2ebf4e9d2c98a80bc1ea30742efe22ac6abbfcc9
zhiliang <jordzhang@gmail.com>
2014-05-29 09:51:42 +0800

    remove flazr target

--- flazr/target/flazr/proxy-start.sh
+++
@@ -1,1 +0,0 @@
-java -cp conf:lib/commons-codec-1.2.jar:lib/flazr-git.jar:lib/httpclient-4.1.2.jar:lib/httpcore-4.1.2.jar:lib/jcl104-over-slf4j-1.4.2.jar:lib/junit-4.8.1.jar:lib/log4j-1.2.14.jar:lib/netty-3.2.7.Final.jar:lib/slf4j-api-1.4.2.jar:lib/slf4j-log4j12-1.4.2.jar -Dflazr.log.suffix=-proxy com.flazr.rtmp.proxy.RtmpProxy $*
